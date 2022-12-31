a43ed5dbef46caf6cd3894db66f4e3f93eae749e
Charles Stein <charles.mst@gmail.com>
2022-12-31 14:33:51 -0300

    refactor cli for code reusability

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -20,79 +20,34 @@ func main() {
 		return
 	}
 	command := args[0]
-	r, err := storage.FindAll(context.Background())
-	if err != nil {
-		fmt.Printf("failed to read %v", err)
-	}
 	switch command {
 
 	case "daemon":
-
 		controller := wakepc.ResolveController()
-
 		daemon := wakepc.NewPcDaemon(&storage, controller)
 		daemon.Start(context.Background())
 	case "ls":
-
-		printState(r)
+		printState(&storage)
 	case "shutdown":
-
-		t := findTargetMac(r, args[1])
-
-		if t.MacAddress == "" {
-			fmt.Println("target not found")
-			return
-		}
-
-		err := storage.Push(context.Background(), t.MacAddress, wakepc.NewShutdownCommand(t.MacAddress))
-		if err != nil {
-			fmt.Printf("failed to push message %v", err)
-			return
-		}
-		fmt.Println("successfully sent shutdown command")
-
+		runOnTarget(&storage, args, wakepc.NewShutdownCommand)
 	case "restart":
-
-		t := findTargetMac(r, args[1])
-
-		if t.MacAddress == "" {
-			fmt.Println("target not found")
-			return
-		}
-
-		err := storage.Push(context.Background(), t.MacAddress, wakepc.NewRestartCommand(t.MacAddress))
-		if err != nil {
-			fmt.Printf("failed to push message %v", err)
-			return
-		}
-		fmt.Println("successfully sent restart command")
-
+		runOnTarget(&storage, args, wakepc.NewRestartCommand)
 	case "wol":
-
-		t := findTargetMac(r, args[1])
-
-		if t.MacAddress == "" {
-			fmt.Println("target not found")
-			return
-		}
-
-		for _, pc := range r {
-			err := storage.Push(context.Background(), pc.MacAddress, wakepc.NewWolCommand(t.MacAddress))
-			if err != nil {
-				fmt.Printf("failed to push message %v", err)
-				return
-			}
-		}
-		fmt.Println("successfully sent wol command to all targets")
+		runOnAllTargets(&storage, args, wakepc.NewWolCommand)
 	default:
 		help()
 
 	}
 }
 func help() {
-	print("usage: ls|shutdown [target]\n")
+	print("usage: ls|shutdown|restart|wol [target]\n")
 }
-func printState(pcs []wakepc.PcState) {
+func printState(storage wakepc.PcStateStorage) {
+
+	pcs, err := storage.FindAll(context.Background())
+	if err != nil {
+		fmt.Printf("failed to read %v", err)
+	}
 
 	for _, pc := range pcs {
 		fmt.Printf("%s\t%s\t%s\n", pc.MacAddress, pc.HostName, pc.RealState())
@@ -108,4 +63,51 @@ func findTargetMac(pcs []wakepc.PcState, target string) wakepc.PcState {
 
 	}
 	return wakepc.PcState{}
+}
+
+func runOnTarget(storage wakepc.PcStateStorage, args []string, commandFunc func(string) wakepc.PcCommandEvent) {
+
+	r, err := storage.FindAll(context.Background())
+	if err != nil {
+		fmt.Printf("failed to read %v", err)
+	}
+
+	t := findTargetMac(r, args[1])
+
+	if t.MacAddress == "" {
+		fmt.Println("target not found")
+		return
+	}
+
+	err = storage.Push(context.Background(), t.MacAddress, commandFunc(t.MacAddress))
+	if err != nil {
+		fmt.Printf("failed to push message %v", err)
+		return
+	}
+	fmt.Printf("successfully sent %s command\n", args[0])
+
+}
+
+func runOnAllTargets(storage wakepc.PcStateStorage, args []string, commandFunc func(string) wakepc.PcCommandEvent) {
+
+	r, err := storage.FindAll(context.Background())
+	if err != nil {
+		fmt.Printf("failed to read %v", err)
+	}
+
+	t := findTargetMac(r, args[1])
+
+	if t.MacAddress == "" {
+		fmt.Println("target not found")
+		return
+	}
+	for _, pc := range r {
+		err := storage.Push(context.Background(), pc.MacAddress, commandFunc(t.MacAddress))
+		if err != nil {
+			fmt.Printf("failed to push message %v", err)
+			return
+		}
+	}
+	fmt.Printf("successfully sent %s command to all targets\n", args[0])
+
 }
