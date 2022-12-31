4ca49eff7374916508380437368c9584b92e125e
Charles Stein <charles.mst@gmail.com>
2022-12-30 23:48:26 -0300

    add pc test

---
+++ pkg/wakepc/pc_test.go
@@ -0,0 +1,28 @@
+package wakepc
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestPc(t *testing.T) {
+
+	storage := PcStateStorageMock{}
+	pc := NewPcDaemon(&storage)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go pc.Start(ctx)
+	time.Sleep(1 * time.Second)
+	cancel()
+
+	result, err := storage.Find(context.Background(), pc.mac)
+	if err != nil {
+		t.Fatalf("got error findig state %v", err)
+	}
+
+	if result.State != Off {
+		t.Fatalf("got wrong state %v", result)
+	}
+
+}
